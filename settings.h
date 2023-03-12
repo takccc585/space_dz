f82a0155ce35289690f640b79aeb6ce155c6c9f8
takccc585 <114169693+takccc585@users.noreply.github.com>
2023-03-12 13:22:18 +0300

    Add files via upload

---
+++ settings.h
@@ -0,0 +1,6 @@
+#pragma once
+const float WINDOW_WIDTH = 1200;
+const float WINDOW_HEIGHT = 900;
+const float FPS = 60.f;
+float speed = 0.0;
+const std::string PLAYER_FILE_NAME = "images\\spaceShips_002.png";
